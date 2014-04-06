b13e09fefc8c43f3623ab5aa1204386f405905e6
Michael Bukosky <mbukosky@gmail.com>
2014-04-06 18:27:23 -0400

    Moving bio page to landing page
    - removing resume partial
    - removing bio header
    - cleaned up routing
    - closes #10

--- src/app/app.js
+++ src/app/app.js
@@ -1,56 +1,53 @@
 var app = angular.module('app', [
-	'ngRoute',
-	'ngSanitize',
-	'ui.bootstrap'
+    'ngRoute',
+    'ngSanitize',
+    'ui.bootstrap'
 ]);
 
-app.config(['$routeProvider', '$locationProvider', 
-	function($routeProvider, $locationProvider) {
-		//$locationProvider.html5Mode(true);
+app.config(['$routeProvider', '$locationProvider',
+    function($routeProvider, $locationProvider) {
+        //$locationProvider.html5Mode(true);
 
-		$routeProvider.
-			when('/', {
-				templateUrl: '/src/app/partials/home.tpl.html',
-				controller: 'CarouselCtrl'
-			}).
-			when('/news', {
-				templateUrl: '/src/app/partials/news.tpl.html',
-				controller: 'BlankCtrl',
-				resolve: {
-					title: function() { 
-						return 'News'; 
-					}
-				}
-			}).
-			when('/resume', {
-				templateUrl: '/src/app/partials/resume.tpl.html',
-				controller: 'ResumeCtrl'
-			}).
-			when('/photos', {
-				templateUrl: '/src/app/partials/blank.tpl.html',
-				controller: 'BlankCtrl',
-				resolve: {
-					title: function() { 
-						return 'Photos'; 
-					}
-				}
-			}).
-			when('/videos', {
-				templateUrl: '/src/app/partials/blank.tpl.html',
-				controller: 'BlankCtrl',
-				resolve: {
-					title: function() { 
-						return 'Videos'; 
-					}
-				}
-			}).
-			otherwise({
-				redirectTo: '/'
-			});	
-}]);
+        $routeProvider.
+        when('/', {
+            templateUrl: '/src/app/partials/home.tpl.html',
+            controller: 'ResumeCtrl'
+        }).
+        when('/news', {
+            templateUrl: '/src/app/partials/news.tpl.html',
+            controller: 'BlankCtrl',
+            resolve: {
+                title: function() {
+                    return 'News';
+                }
+            }
+        }).
+        when('/photos', {
+            templateUrl: '/src/app/partials/blank.tpl.html',
+            controller: 'BlankCtrl',
+            resolve: {
+                title: function() {
+                    return 'Photos';
+                }
+            }
+        }).
+        when('/videos', {
+            templateUrl: '/src/app/partials/blank.tpl.html',
+            controller: 'BlankCtrl',
+            resolve: {
+                title: function() {
+                    return 'Videos';
+                }
+            }
+        }).
+        otherwise({
+            redirectTo: '/'
+        });
+    }
+]);
 
-app.run(function ($rootScope, $location) {
-    $rootScope.$on('$routeChangeSuccess', function(){
+app.run(function($rootScope, $location) {
+    $rootScope.$on('$routeChangeSuccess', function() {
         ga('send', 'pageview', $location.path());
     });
 });

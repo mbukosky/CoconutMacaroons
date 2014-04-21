bef5919dd15669a753be441b22cfd191f4429d23
Michael Bukosky <mbukosky@gmail.com>
2014-04-21 16:48:01 -0400

    #13: Filled in videos
    - closes #13

--- src/app/controllers/videos.ctrl.js
+++ src/app/controllers/videos.ctrl.js
@@ -1,5 +1,12 @@
 angular.module('app').controller('VideosCtrl', ['$scope',
-	function($scope){
+    function($scope) {
 
-	/* Code Here */
-}]);
+        $scope.videos = [{
+            path: 'http://vimeo.com/87091260',
+            thumbnail: '/src/assets/img/photos/news/media/new_reel.jpg'
+        }, {
+            path: 'http://www.youtube.com/embed/qf9M-WJPzgk',
+            thumbnail: '/src/assets/img/photos/news/media/spaceship.jpg'
+        }];
+    }
+]);

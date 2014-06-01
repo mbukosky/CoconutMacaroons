138decdd165beb3244e07379a47863de696aaf32
lmeley <lmeley@gmail.com>
2014-05-31 21:03:52 -0400

    added video descriptions

--- src/app/controllers/videos.ctrl.js
+++ src/app/controllers/videos.ctrl.js
@@ -4,23 +4,23 @@ angular.module('app').controller('VideosCtrl', ['$scope',
         $scope.videos = [{
             path: 'http://vimeo.com/87091260',
             thumbnail: '/src/assets/img/photos/video/kings.jpg',
-            caption: 'Lorem ipsum dolor sit amet.'
+            caption: 'Dramatic Reel (2014)'
         }, {
             path: 'http://youtu.be/qf9M-WJPzgk',
             thumbnail: '/src/assets/img/photos/video/spaceship_teaser.jpg',
-            caption: 'Lorem ipsum dolor sit amet.'
+            caption: 'THE SPACESHIP Teaser (2014)'
         }, {
             path: 'http://youtu.be/Ei7mnO9GRHc',
             thumbnail: '/src/assets/img/photos/video/starwars.jpg',
-            caption: 'Lorem ipsum dolor sit amet.'
+            caption: 'Star Wars VII Audition Video  -  yep, I did it!'
         }, {
             path: 'http://youtu.be/feFac2oLL6M',
             thumbnail: '/src/assets/img/photos/video/cov.jpg',
-            caption: 'Lorem ipsum dolor sit amet.'
+            caption: 'CAROLINE OF VIRGINIA  -  Full Movie'
         }, {
             path: 'http://youtu.be/s-qEA4jW-Pk',
             thumbnail: '/src/assets/img/photos/video/speedreel.jpg',
-            caption: 'Lorem ipsum dolor sit amet.'
+            caption: 'Speed Reel. Compilation of projects from 2007 - 2010.'
         }];
     }
 ]);
